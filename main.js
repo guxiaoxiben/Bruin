93168a196c3043b4bb61ad6fd30d1f74c3b52821
顾小夕 <2504408987>
2023-02-06 10:10:05 +0800

    配置网络请求、请求轮播图数据

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 import App from './App'
 
+// 导入网络请求包
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+// 配置请求根路径
+$http.baseUrl = 'https://www.uinav.com'
+// 请求拦截器
+$http.beforeRequest = function (options) {
+	uni.showLoading({
+		title:"数据加载中..."
+	})
+}
+// 响应拦截器
+$http.afterRequest = function () {
+	uni.hideLoading()
+}
+
 // #ifndef VUE3
 import Vue from 'vue'
 Vue.config.productionTip = false
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -8,8 +8,25 @@
 	export default {
 		data() {
 			return {
-				
+				// 轮播图数据
+				swiperList:[]
 			};
+		},
+		onLoad() {
+			this.getSwiperList()
+		},
+		methods:{
+			async getSwiperList(){
+				const { data: res } = await uni.$http.get('/api/public/v1/home/swiperdata')
+				if(res.meta.status!==200){
+					return uni.showToast({
+						title:'数据获取失败！',
+						duration:1500,
+						icon:'none'
+					})
+				}
+				this.swiperList = res.message
+			}
 		}
 	}
 </script>
