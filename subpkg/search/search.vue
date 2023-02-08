4361ca2c7b4925a6d8bb38fc0ea0d1ffc6301715
顾小夕 <2504408987@qq.com>
2023-02-08 17:21:18 +0800

    搜索框防抖处理

--- subpkg/search/search.vue
+++ subpkg/search/search.vue
@@ -8,14 +8,23 @@
 	export default {
 		data() {
 			return {
-				
+				// 延时器
+				timer:null,
+				// 关键词
+				kk:''
 			};
 		},
 		methods:{
 			input(e) {
-			    // e.value 是最新的搜索内容
-			    console.log(e)
-			  }
+			  // 清除 timer 对应的延时器
+			  clearTimeout(this.timer)
+			  // 重新启动一个延时器，并把 timerId 赋值给 this.timer
+			  this.timer = setTimeout(() => {
+			    // 如果 500 毫秒内，没有触发新的输入事件，则为搜索关键词赋值
+			    this.kk = e
+			    console.log(this.kk)
+			  }, 500)
+			}
 		}
 	}
 </script>
