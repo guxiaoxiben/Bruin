1c7f7e096a9d32066456616bc29a24d772fa4c40
顾小夕 <2504408987@qq.com>
2023-02-09 14:54:16 +0800

    商品信息渲染、修复图片之间间隙和苹果端显示问题

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -6,6 +6,27 @@
 		    <image :src="item.pics_big" @click="preview(index)"></image>
 		  </swiper-item>
 		</swiper>
+		
+		<!-- 商品信息区域 -->
+		<view class="goods-info-box">
+			<!-- 商品价格 -->
+			<view class="price">￥{{goods_info.goods_price}}</view>
+			<!-- 商品信息主题 -->
+			<view class="goods-info-body">
+				<!-- 商品名称 -->
+				<view class="goods-name">{{goods_info.goods_name}}</view>
+				<!-- 收藏 -->
+				<view class="favi">
+					<uni-icons type="star" size="18" color="gray"></uni-icons>
+				    <text>收藏</text>
+				</view>
+			</view>
+			<!-- 商品运费 -->
+			<view class="yf">快递：免运费</view>
+		</view>
+		
+		<!-- 商品详情信息 -->
+		<rich-text :nodes="goods_info.goods_introduce"></rich-text>
 	</view>
 </template>
 
@@ -22,6 +43,9 @@
 			async getGoodsDetail(goods_id) {
 				const { data: res } = await uni.$http.get('/api/public/v1/goods/detail', { goods_id })
 			    if (res.meta.status !== 200) return uni.$showMsg()
+				// 使用字符串的 replace() 方法，为 img 标签添加行内的 style 样式，从而解决图片底部空白间隙的问题
+				// 使用字符串的 replace() 方法，将 webp 的后缀名替换为 jpg 的后缀名
+				res.message.goods_introduce = res.message.goods_introduce.replace(/<img /g, '<img style="display:block;" ').replace(/webp/g, 'jpg')
 			    // 为 data 中的数据赋值
 			    this.goods_info = res.message
 			},
@@ -55,6 +79,45 @@ swiper {
   image {
     width: 100%;
     height: 100%;
+  }
+}
+// 商品信息区域的样式
+.goods-info-box {
+  padding: 10px;
+  padding-right: 0;
+
+  .price {
+    color: #c00000;
+    font-size: 18px;
+    margin: 10px 0;
+  }
+
+  .goods-info-body {
+    display: flex;
+    justify-content: space-between;
+
+    .goods-name {
+      font-size: 13px;
+      padding-right: 10px;
+    }
+    // 收藏区域
+    .favi {
+      width: 120px;
+      font-size: 12px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      border-left: 1px solid #efefef;
+      color: gray;
+    }
+  }
+
+  // 运费
+  .yf {
+    margin: 10px 0;
+    font-size: 12px;
+    color: gray;
   }
 }
 </style>
