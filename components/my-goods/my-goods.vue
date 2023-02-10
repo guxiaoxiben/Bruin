ea2d7d1dd3cebc7ae405362b155542a5aa814d02
顾小夕 <2504408987@qq.com>
2023-02-10 10:29:00 +0800

    购物车页面 -渲染标题、商品列表基本结构、rodio勾选动态设置动态

--- components/my-goods/my-goods.vue
+++ components/my-goods/my-goods.vue
@@ -2,6 +2,7 @@
   <view class="goods-item">
     <!-- 商品左侧图片区域 -->
     <view class="goods-item-left">
+	  <radio :checked="goods.goods_state" color="#C00000" v-if="showRadio"></radio>
       <image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
     </view>
     <!-- 商品右侧信息区域 -->
@@ -25,6 +26,12 @@
         type: Object,
         defaul: {},
       },
+	  // 是否展示图片左侧的 radio
+	  showRadio: {
+	    type: Boolean,
+	    // 如果外界没有指定 show-radio 属性的值，则默认不展示 radio 组件
+	    default: false,
+	  },
     },
     data() {
       return {
@@ -48,7 +55,10 @@
 
     .goods-item-left {
       margin-right: 5px;
-
+	  display: flex;
+	  justify-content: space-between;
+	  align-items: center;
+	  
       .goods-pic {
         width: 100px;
         height: 100px;
--- pages/cart/cart.vue
+++ pages/cart/cart.vue
@@ -1,14 +1,29 @@
 <template>
 	<view>
-		购物车
+		<!-- 购物车商品列表的标题区域 -->
+		<view class="cart-title">
+			<!-- 左侧的图标 -->
+			<uni-icons type="shop" size="18"></uni-icons>
+			<!-- 描述文本 -->
+			<text class="cart-title-text">购物车</text>
+		</view>
+		
+		<!-- 商品列表区域 -->
+		<block v-for="(goods, i) in cart" :key="i">
+			<my-goods :goods="goods" :show-radio="true"></my-goods>
+		</block>
 	</view>
 </template>
 
 <script>
 	import badgeMix from '@/mixins/tabbar-badge.js'
+	import { mapState } from 'vuex'
 
 	export default {
 		mixins:[badgeMix],
+		computed:{
+			...mapState('m_cart',['cart'])
+		},
 		data() {
 			return {
 				
@@ -18,5 +33,15 @@
 </script>
 
 <style lang="scss">
-
+.cart-title {
+  height: 40px;
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  padding-left: 5px;
+  border-bottom: 1px solid #efefef;
+  .cart-title-text {
+    margin-left: 10px;
+  }
+}
 </style>
