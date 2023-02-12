001362973d4828294d247bf047bb0f62bfd26f3c
顾小夕 <2504408987@qq.com>
2023-02-12 12:02:39 +0800

    创建组件、渲染页面、获取用户信息、储存数据到vuex

--- components/my-settle/my-settle.vue
+++ components/my-settle/my-settle.vue
@@ -12,19 +12,22 @@
 		</view>
 
 		<!-- 结算按钮 -->
-		<view class="btn-settle">结算({{checkedCount}})</view>
+		<view class="btn-settle" @click="settlement">结算({{checkedCount}})</view>
 	</view>
 </template>
 
 <script>
 	import {
 		mapGetters,
-		mapMutations
+		mapMutations,
+		mapState
 	} from 'vuex'
 
 	export default {
 		computed: {
 			...mapGetters('m_cart', ['checkedCount', 'total', 'checkedGoodsAmount']),
+			...mapGetters('m_user', ['addstr']),
+			...mapState('m_user', ['token']),
 			// 判断是否全选
 			isFullCheck() {
 				return this.total === this.checkedCount
@@ -39,6 +42,17 @@
 				// 修改购物车中所有商品的选中状态
 				// !this.isFullCheck 表示：当前全选按钮的状态取反之后，就是最新的勾选状态
 				this.updateAllGoodsState(!this.isFullCheck)
+			},
+			// 点击了结算按钮
+			settlement() {
+				// 1. 先判断是否勾选了要结算的商品
+				if (!this.checkedCount) return uni.$showMsg('请选择要结算的商品！')
+
+				// 2. 再判断用户是否选择了收货地址
+				if (!this.addstr) return uni.$showMsg('请选择收货地址！')
+
+				// 3. 最后判断用户是否登录了
+				if (!this.token) return uni.$showMsg('请先登录！')
 			}
 		}
 	}
--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,17 +1,28 @@
 <template>
 	<view>
-		我的
+		<!-- 用户未登录时，显示登录组件 -->
+		<my-login v-if="!token"></my-login>
+
+		<!-- 用户登录后，显示用户信息组件 -->
+		<my-userinfo v-else></my-userinfo>
 	</view>
 </template>
 
 <script>
 	import badgeMix from '@/mixins/tabbar-badge.js'
-	
+	import {
+		mapState
+	} from 'vuex'
+
 	export default {
-		mixins:[badgeMix],
+		mixins: [badgeMix],
+		computed: {
+			// 2. 从 m_user 模块中导入需要的 token 字符串
+			...mapState('m_user', ['token']),
+		},
 		data() {
 			return {
-				
+
 			};
 		}
 	}
