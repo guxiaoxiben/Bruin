e9848eaa4f44a46146027a0648d8a717a86c01ef
顾小夕 <2504408987@qq.com>
2023-02-12 09:04:58 +0800

    修复苹果端授权失败

--- components/my-address/my-address.vue
+++ components/my-address/my-address.vue
@@ -60,7 +60,8 @@
 					this.updateAddress(succ)
 				}
 				// 3. 用户没有授权
-				if (err && err.errMsg === 'chooseAddress:fail auth deny') {
+				if (err && (err.errMsg === 'chooseAddress:fail auth deny' || err.errMsg ===
+						'chooseAddress:fail authorize no response')) {
 					this.reAuth() // 调用 this.reAuth() 方法，向用户重新发起授权申请
 				} /*  */
 			},
